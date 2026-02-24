aa98568fae0512e711d0dd7c90f510e73b7dece7
hyeongcheol <dlgudcjf99@gmail.com>
2026-02-24 18:04:34 +0900

    db

---
+++ pro1/pack3/db6quiz4.py
@@ -0,0 +1,38 @@
+# 문3) 성별 직원 현황 출력 : 성별(남/여) 단위로 직원 수와 평균 급여 출력
+# 성별 직원수 평균급여
+# 남 3 8500
+# 여 2 7800
+import MySQLdb
+import pickle
+
+with open('mydb.dat', mode='rb') as obj:
+    config = pickle.load(obj)
+
+def chulbal():
+    try:
+        conn = MySQLdb.connect(**config)
+        cursor = conn.cursor()
+
+
+        sql = """
+        select jikwonno 직원번호, jikwonname 직원명, count(gogekno) 관리고객수
+        from jikwon
+        right outer join gogek on jikwonno = gogekdamsano
+        group by jikwonno
+        """
+        cursor.execute(sql)
+        datas = cursor.fetchall()
+
+        for a,b,c in datas:
+            print(a,b,c)
+
+
+    except Exception as e:
+        print('err :', e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+if __name__=="__main__":
+    chulbal()
